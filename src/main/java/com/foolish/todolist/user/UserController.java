78f5ede85f7f8eb022e98db39bc597c9c5239f0b
foolish8932 <contasuch123@gmail.com>
2023-10-14 02:21:40 -0300

    implements: database & user repository

--- src/main/java/com/foolish/todolist/user/UserController.java
+++ src/main/java/com/foolish/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package com.foolish.todolist.user;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @PostMapping("/")
-    public void create(@RequestBody UserModel usermodel) {
-        System.out.println(usermodel.name);
+    @Autowired
+    private IUserRepository userRepository;
+    @PostMapping()
+    public ResponseEntity create(@RequestBody UserModel usermodel) {
+        UserModel user = this.userRepository.findByUsername(usermodel.getUsername());
+        if(user != null) {
+            System.out.println("cant create");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists.");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(usermodel));
     }
 }
