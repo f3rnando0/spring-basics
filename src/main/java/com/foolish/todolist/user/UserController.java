dd29335aa379100c9d069a75374d2bc1e5eb7319
foolish8932 <contasuch123@gmail.com>
2023-10-15 02:38:15 -0300

    implements: task route & auth middleware

--- src/main/java/com/foolish/todolist/user/UserController.java
+++ src/main/java/com/foolish/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package com.foolish.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,12 +16,16 @@ public class UserController {
     @Autowired
     private IUserRepository userRepository;
     @PostMapping()
-    public ResponseEntity create(@RequestBody UserModel usermodel) {
-        UserModel user = this.userRepository.findByUsername(usermodel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        UserModel user = this.userRepository.findByUsername(userModel.getUsername());
         if(user != null) {
             System.out.println("cant create");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists.");
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(usermodel));
+
+        var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(hashedPassword);
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(userModel));
     }
 }
